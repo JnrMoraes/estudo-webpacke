d49e002927ad34d013ab5682c4f3e5b2309e753a
juniormoraes <contatomoraesjr@gmail.com>
2021-03-05 14:52:40 -0300

    incluindo build de produção e minificar o bundle de desenvolvimento

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,19 +1,29 @@
 const path = require('path');
+const babliliPlugin = require('babel-minify-webpack-plugin');
+
+let plugins = [];
+
+if (process.env.NODE_ENV == 'production') {
+
+  plugins.push(new babliliPlugin());
+}
+
 module.exports = {
-    entry: './app-src/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    }
+  entry: './app-src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins
 }
